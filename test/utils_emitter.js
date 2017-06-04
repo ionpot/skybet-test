d85b6f4fb7a57dac71849d661345bdbe506599b0
Emir Kotan <emirkotan@gmail.com>
2017-06-04 02:43:21 +0100

    Improve test of utils/emitter.

--- test/utils_emitter.js
+++ test/utils_emitter.js
@@ -6,8 +6,9 @@ var sut = require("utils/emitter");
 
 suite("Utils - Emitter");
 
-test("ok", function (done) {
-	this.timeout(500);
+test("ok", function () {
+	var i = 0;
+	var j = 0;
 
 	function Foo(a, b) {
 		this.a = a;
@@ -18,14 +19,28 @@ test("ok", function (done) {
 		this.emit("name", this.a, this.b);
 	});
 
-	var foo = new Foo("1", 2);
+	var foo1 = new Foo("1", 2);
+	var foo2 = new Foo("3", 4);
 
-	foo.on("name", function (a, b) {
+	foo1.on("name", function (a, b) {
 		A.strictEqual(a, "1");
 		A.strictEqual(b, 2);
 
-		done();
+		i += 1;
 	});
 
-	foo.run();
+	foo2.once("name", function (a, b) {
+		A.strictEqual(a, "3");
+		A.strictEqual(b, 4);
+
+		j += 1;
+	});
+
+	foo1.run();
+	foo1.run();
+	foo2.run();
+	foo2.run();
+
+	A.strictEqual(i, 2);
+	A.strictEqual(j, 1);
 });
