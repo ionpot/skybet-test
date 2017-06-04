51c5fb905796342c8976c16d7d445947fd943300
Emir Kotan <emirkotan@gmail.com>
2017-06-04 20:34:36 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var http = require("http");
+
+var Router = require("./router.js");
+
+var Err = require("./ctrl/error.js");
+var parse = require("./parse/url.js");
+
+var port = +process.argv[2] || 8080;
+
+http.createServer(function (req, res) {
+	var parts;
+
+	try {
+		parts = parse(req.url);
+
+		switch (req.method) {
+		case "GET":
+			return Router.get(parts, res);
+
+		case "POST":
+			return Router.post(parts, res);
+
+		default:
+			return Err.notImplemented(res);
+		}
+
+	} catch (e) {
+		return Err.badRequest(res);
+	}
+
+}).listen(port);
