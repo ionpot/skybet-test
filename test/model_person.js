a8eef87e6e561b8357c209a8a1e22f3a385846bc
Emir Kotan <emirkotan@gmail.com>
2017-06-04 18:45:42 +0100

    test/model_person: Test for missing first/last names.

--- test/model_person.js
+++ test/model_person.js
@@ -4,21 +4,30 @@ var A = require("assert");
 
 var SUT = require("../model/person.js");
 
-var first = "Emir";
-var last = "Kotan";
-
-var sut = SUT.create(first, last);
-
 suite("Model - Person");
 
-test("#first", function () {
-	A.strictEqual(sut.first, first);
+test("ok", function () {
+	var sut = SUT.create("A", "B");
+
+	A.strictEqual(sut.first, "A");
+	A.strictEqual(sut.last, "B");
+	A.strictEqual(sut.toString(), "A B");
 });
 
-test("#last", function () {
-	A.strictEqual(sut.last, last);
+test("no first", function () {
+	var sut = SUT.create("", "B");
+
+	A.strictEqual(sut.toString(), "B");
 });
 
-test("#toString()", function () {
-	A.strictEqual(sut.toString(), "Emir Kotan");
+test("no last", function () {
+	var sut = SUT.create("A", "");
+
+	A.strictEqual(sut.toString(), "A");
+});
+
+test("empty", function () {
+	var sut = SUT.create("", "");
+
+	A.strictEqual(sut.toString(), "");
 });
