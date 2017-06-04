6d4d5c85d7f9b6cef531ea0ef99ec871d2b36c62
Emir Kotan <emirkotan@gmail.com>
2017-06-04 19:17:54 +0100

    Test parse_people changes to parse_names with more checks.

---
+++ test/parse_names.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var A = require("assert");
+var QS = require("querystring");
+
+var sut = require("../parse/names.js");
+
+suite("Parse - Names");
+
+test("string ok", function () {
+	var list = sut(QS.stringify({
+		firstname: "A",
+		surname: "B"
+	}));
+
+	A.strictEqual(list.length, 1);
+
+	A.strictEqual(list[0].toString(), "A B");
+});
+
+test("array ok", function () {
+	var list = sut(QS.stringify({
+		firstname: ["A", "C", "E"],
+		surname: ["B", "D", "F"]
+	}));
+
+	A.strictEqual(list.length, 3);
+
+	A.strictEqual(list[0].toString(), "A B");
+	A.strictEqual(list[1].toString(), "C D");
+	A.strictEqual(list[2].toString(), "E F");
+});
+
+test("empty list on bogus input", function () {
+	var list = sut("@#$%^&*");
+
+	A.strictEqual(list.length, 0);
+});
+
+test("empty list on mismatch", function () {
+	var list = sut(QS.stringify({
+		firstname: ["A", "C", "E"],
+		surname: ["B"]
+	}));
+
+	A.strictEqual(list.length, 0);
+});
